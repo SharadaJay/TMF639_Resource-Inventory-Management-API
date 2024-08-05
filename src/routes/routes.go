87e0e8a6ae4e47f001151c10a90af405d6baf0cc
SharadaJay <sharada.prabash@gmail.com>
2024-08-06 00:24:45 +0300

    add CORS middleware

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -7,14 +7,33 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"resource-inventory/src/handlers"
 )
 
 type Routes struct {
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func (c Routes) StartGin() {
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	resourceApi := r.Group("/tmf-api/resourceInventoryManagement/v4/")
 	{
 		resourceApi.GET("/resource", handlers.ListResource)
@@ -25,6 +44,6 @@ func (c Routes) StartGin() {
 	}
 	err := r.Run()
 	if err != nil {
-		return
+		log.Fatalf("Failed to run gin router")
 	}
 }
